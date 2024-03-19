1027d729056f1e2cdaceb5f306575606b688af16
Guilherme Avelino <guilhermeavelino1@outlook.com>
2024-03-19 09:36:52 -0300

    fix: adjusting imports

--- app/src/main/java/br/com/fiap/pontosverdes/MainActivity.kt
+++ app/src/main/java/br/com/fiap/pontosverdes/MainActivity.kt
@@ -3,38 +3,13 @@ package br.com.fiap.pontosverdes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.com.fiap.pontosverdes.screens.PaginaFinal
 import br.com.fiap.pontosverdes.screens.PaginaInicial
+import br.com.fiap.pontosverdes.screens.PaginaSecundaria
+import br.com.fiap.pontosverdes.screens.PaginaFinal
 import br.com.fiap.pontosverdes.ui.theme.PontosVerdesTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,13 +17,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PontosVerdesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     PaginaInicial()
-                    // PaginaFinal()
                 }
             }
         }
